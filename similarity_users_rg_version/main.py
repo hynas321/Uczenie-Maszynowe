679f9ae0d33ceff72ab12c0961c61fd753e11cc9
rafalgorniak <234753@edu.p.lodz.pl>
2024-12-04 12:27:31 +0100

    similarity for users seems to be done

--- similarity_users_rg_version/main.py
+++ similarity_users_rg_version/main.py
@@ -1,36 +1,19 @@
+from similarity_users_rg_version.similarity.cross_validation import cross_validate
+from similarity_users_rg_version.similarity.predict_values import predict_values
 from similarity_users_rg_version.utils.csv_reader import load_csv_data
-from similarity_users_rg_version.utils.similarity import calculate_users_similarity, assess_predictions, get_accuracy
+from similarity_users_rg_version.utils.csv_saver import save_data_to_csv
 from similarity_users_rg_version.utils.users_creation import generate_users
 
 
 def main():
     train_data, test_data = load_csv_data()
-    users = generate_users(train_data, test_data)
+    users = generate_users(train_data=train_data, task_data=test_data)
 
-    all_accuracy = []
+    # cross_validate(users=users, threshold=72)
 
-    for user in users:
+    predict_values(users=users)
 
-        similarities_with_user = []
-
-        for user_ in users:
-            users_similarity = calculate_users_similarity(user, user_)
-            similarities_with_user.append([users_similarity, user_])
-
-        similarities_with_user.sort(key=lambda x: x[0])
-        users_table = [user_[1] for user_ in similarities_with_user]
-        users_table = users_table[:len(users_table) - 1]
-
-        for user__ in users_table:
-            assess_predictions(user, user__)
-
-        # user.show_train_set()
-        accuracy = 100*get_accuracy(user)
-
-        all_accuracy.append(accuracy)
-        print(f'For user with id: {user.id}, the accuracy is: {accuracy:.1f} %')
-
-    print(f'Overall accuracy: {(sum(all_accuracy) / len(all_accuracy)):.1f} %')
+    save_data_to_csv(users=users)
 
 if __name__ == '__main__':
     main()
--- similarity_users_rg_version/utils/similarity.py
+++ similarity_users_rg_version/utils/similarity.py
@@ -1,12 +1,11 @@
 from similarity_users_rg_version.classes.user import User
 
 
-def calculate_users_similarity(user1: User, user2: User) -> float:
+def calculate_users_similarity(user1: User, user2: User, threshold: int = None) -> float:
     same_movies = 0
     same_rating = 0
 
-    for movie in user1.train_set[:72]:
-
+    for movie in user1.train_set[:threshold]:
         for movie_ in user2.train_set:
 
             if movie.id == movie_.id:
@@ -19,22 +18,29 @@ def calculate_users_similarity(user1: User, user2: User) -> float:
     return same_rating / same_movies
 
 
-def assess_predictions(user1: User, user2: User):
-    for movie in user1.train_set[72:]:
-
+def assess_training_predictions(user1: User, user2: User, threshold: int = None):
+    for movie in user1.train_set[threshold:]:
         for movie_ in user2.train_set:
 
             if movie.id == movie_.id:
-                # print(f'For movie {movie.id}, previous rating is {movie.predicted_rating} and new rating is {movie_.rating}')
                 movie.predicted_rating = movie_.rating
                 break
 
 
-def get_accuracy(user: User):
+def assess_testing_predictions(user1: User, user2: User):
+    for movie in user1.test_set:
+        for movie_ in user2.train_set:
+
+            if movie.id == movie_.id:
+                movie.rating = movie_.rating
+                break
+
+
+def get_accuracy(user: User, threshold: int = None):
     same_movies = 0
     same_rating = 0
 
-    for movie in user.train_set[72:]:
+    for movie in user.train_set[threshold:]:
         same_movies += 1
         if movie.rating == movie.predicted_rating:
             same_rating += 1
